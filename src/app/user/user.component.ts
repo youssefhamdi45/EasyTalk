b692c015cf2b86bfcfd2b5b61d42024c24a8c002
youssefhamdi45 <yosefhamdi08@gmail.com>
2025-10-25 15:45:00 +0100

    delete signal from user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input , input} from '@angular/core';
+import { Component , EventEmitter, Input , Output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length )
 @Component({
@@ -9,14 +9,12 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length )
   styleUrl: './user.component.css'
 })
 export class UserComponent {
- // @Input({required: true}) avatar! : string;
- // @Input({required: true}) name! : string;
-  avatar = input.required<string>();
-  name = input.required<string>();
-  imagePath
-  //get imagePath(){
-  //return 'assets/users/'+this.avatar;
-  //}
+  @Input({required: true}) avatar! : string;
+  @Input({required: true}) name! : string;
+  @Output()select = new EventEmitter();
+  get imagePath(){
+  return 'assets/users/'+this.avatar;
+  }
    onSelectUser(){
      
     }
